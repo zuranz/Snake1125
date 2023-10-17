19a1647caf252f12a34da940450b3fa795134481
Morty279 <mrbt490@gmaio.com>
2023-10-18 02:19:30 +1000

    Реализация CheckSnakeIntersect

--- ConsoleApp18/CheckSnakeIntersect.cs
+++ ConsoleApp18/CheckSnakeIntersect.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +12,12 @@ namespace ConsoleApp18
     {
         private static bool CheckSnakeIntersect()
         {
+            int a = snake[0][0];
+            int b = snake[0][1];
+
             for (int i = 1; i < snake.Count; i++)
             {
-                if (snake[0] == snake[i])
+                if (snake[i][0] == a && snake[i][1] == b)
                     return true;// вернуть true, если координаты нулевой ячейки змейки совпадают с одной из других ее ячеек
             }
             return false;
