ea0d8cdc722bc4e3401a96335b43484408c88925
Student <Student@CAB201-12>
2023-10-18 11:04:06 +1000

    Реализация InitSnake

--- ConsoleApp18/InitSnake.cs
+++ ConsoleApp18/InitSnake.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleApp18
+﻿using System.Collections.Generic;
+namespace ConsoleApp18
 {
     partial class Program
     {
@@ -8,6 +9,12 @@
             // добавить в коллекцию ячейку со значением {10, 10}. Это будут начальные координаты змейки
             // direction установить направо
             // speed назначить в 250 мс. это скорость движения змейки
+
+            List<int> snake = new List<int>();
+            List<int> znachenya = new List<int> { 10, 10 };
+            snake.AddRange(znachenya);
+            direction = 2;
+            speed = 250;
         }
     }
 }
